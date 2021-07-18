74cb3a2f2d2af162ad322fffd166e264c5a37342
BlueT04 <Blue-T04@zmwaresec.com>
2021-07-18 16:05:51 +0100

    Basic endpoints

--- server.js
+++ server.js
@@ -1,14 +1,25 @@
 const express = require('express')
 const app = express()
 const PORT = process.env.PORT || 5000
-
+app.use(express.json)
 
 // Home route
 app.get('/', (req, res)=>{
     res.send('Main route')
 })
 
+app.get('/post', (req, res)=>{
+    res.send({
+        "message": "post 01 made by Blue-T04"
+    })
+})
 
+app.post('/post/:id', (req, res)=>{
+    const {id} = req.params
+    res.send({
+        post: `new post added | id : ${id}` 
+    })
+})
 
 
 
